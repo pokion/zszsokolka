88dbb1ff1811b1bd0c69524ebd23fd0305cb5aca
PatrykOlechno <patrykoolechno@gmail.com>
2019-02-28 13:30:20 +0100

    czarek

--- js/creator.js
+++ js/creator.js
@@ -1,17 +1,22 @@
-let imgUpl = new FormData();
+let images = [];
+
 function readURL(input) {
+	console.log($('input[type=file]').prop('files'),input.target.files);
 	if (input.target.files && input.target.files[0]) {
 		for(let i=0;i<input.target.files.length;i++){
 			var reader = new FileReader();
 
 		    reader.onload = function(e) {
+		    	console.log(e)
+		    	images.push({name: input.target.files[i].name,
+		    				 data: e.target.result});
 		    	let imageRep = $(`<a href="${e.target.result}" data-lightbox="roadtrip" class="imageStyle"><img src="${e.target.result}" /></a>`);
 		    	let $img = $(`<div class="card">
 								<img src="${e.target.result}">
 								<div class="card-panel grey lighten-5 z-depth-1 center-align">
 									<p class="truncate">${input.target.files[i].name}</p>
 								</div>
-								<a class="btn-floating btn-small waves-effect waves-light rem red"><i class="material-icons">remove_circle_outline</i></a>
+								<a class="btn-floating btn-small waves-effect waves-light rem red" image="${input.target.files[i].name}"><i class="material-icons">remove_circle_outline</i></a>
 							</div>
 						`);
 		    	
@@ -19,27 +24,21 @@ function readURL(input) {
 		    	$('div .replace').append(imageRep);
 				/*remove img*/
 		    		jQuery('.rem').click((e)=>{
-						$(this).remove();
+						/*$(e.currentTarget).parent().remove();*/
 						console.log('no chyba działa');
-						console.log(input.target.files)
+						let nameImg = $(e.currentTarget).attr('image');
+						images.forEach((elem,index)=>{
+							if(elem.name == nameImg) console.log(elem, index)
+						})
 					})
 				/*remove img*/
-				imgUpl.append('image','tutaj ma być blob uzyskany z events lub imput',input.target.files[i].name)
-				console.log(imgUpl)
 			}
 
 		    reader.readAsDataURL(input.target.files[i]);
 		    
-
 		}
-
 	}
-
-
 }
-
-	
-
 
 
 $(document).ready(function(){
