651ee56ed0c83d3d723e2d3ffae20f659ffe6933
pokion <pokion@outlook.com>
2019-04-03 14:43:02 +0200

    nowy wygląd posta
    :+1:

--- html/index.php
+++ html/index.php
@@ -20,112 +20,73 @@
 				<h5 class="light grey-text text-lighten-3 tresc">Here's our small slogan.</h5>
 			</div>
 			<li>
-				<img class="sliderCreator" src="https://farm8.staticflickr.com/7871/46565470735_978b5348d6_c.jpg"> <!-- random image -->
+				<img class="sliderCreator" src="../images/1553619764_46506569985_c5025ff3e3_c.jpg"> <!-- random image -->
+			</li>
+			<li>
+				<img class="sliderCreator" src="../images/1553619764_46506570625_9151b1bc43_c.jpg"> <!-- random image -->
 			</li>
 		</ul>
 	</div>
 	<div class="container">
-		<div class="row full">
-			<div class="col s12 card white-text kuba">
-				<img src="../images/1553619764_33545915408_e1babf3c7b_c.jpg" class="post">
-				<h3 class="letterSpac">Staże zawodowe we Frankfurcie w ramach projektu Erasmus+</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac"></h5>W dniach 10-23 marca 2019 roku w ramach programu Unii Europejskiej Erasmus+ Kształcenie i szkolenia zawodowe projekt "Nowe perspektywy po zagranicznym stażu zawodowym młodych cukierników, kucharzy i mechaników pojazdów samochodowych" uczennica klasy I wz Gabriela Różak wzięła udział w dwutygodniowym stażu w zawodzie kucharz we Frankfurcie nad Odrą, zorganizowanym przez Podlaską Wojewódzką Komendę Ochotniczych Hufców Pracy w Białymstoku.
 
-Celem projektu jest podniesienie kwalifikacji zawodowych 70 uczestników poprzez zdobycie dodatkowych kompetencji oraz nowego doświadczenia warsztatowego za granicą. Równie ważny jest rozwój osobisty uczestników, poszerzenie horyzontów światopoglądowych, poznanie kultury pracy w Niemczech, jak też poznanie przez młodzież niemieckiego języka zawodowego. Uczniowie odbywają praktyki na nowocześnie wyposażonych warsztatach w małych grupach pod okiem doświadczonych instruktorów zgodnie z metodologią niemiecką, co zapewni w rezultacie wysoki poziom zdobytej wiedzy i umiejętności, potwierdzone certyfikatem organizacji goszczącej BWF oraz dokumentem Europass-Mobility.
-
-Gabriela uczestniczyła w stażu dzięki współpracy Zespołu Szkół Zawodowych w Sokółce z Ośrodkiem Szkolenia i Wychowania OHP w Wasilkowie.
+		<div class="row">
+			<div class="col s12 m12 6 l12 xl6">
+				<div class="card">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img src="../img/gos.jpg" class="activator">
+					</div>
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">
+							Wizyta uczniów Szkoły Podstawowej z Szudziałowa.
+						</span>
+						<p>22 marca 2019 roku Zespół Szkół Zawodowych im. Elizy Orzeszkowej w Sokółce odwiedziła młodzież ze Szkoły Podstawowej im. Bohaterów Powstań Narodowych i Gimnazjum im. Powstańców Styczniowych w Szudziałowie. Pan dyrektor Grzegorz Zalewski wraz z wicedyrektorem Lechem Czarnowiczem...</p>
+					</div>
+					<div class="card-action">
+						<a href="#">Czytaj dalej.</a>
+					</div>
+				</div>
 			</div>
-			<div class="col s12 card white-text kuba">
-				<img src="https://farm8.staticflickr.com/7920/47480641671_dc6395b781_s.jpg" class="post">
-				<h3 class="letterSpac">Wizyta uczniów Szkoły Podstawowej z Szudziałowa</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac">22 marca 2019 roku Zespół Szkół Zawodowych im. Elizy Orzeszkowej w Sokółce odwiedziła młodzież ze Szkoły Podstawowej im. Bohaterów Powstań Narodowych i Gimnazjum im. Powstańców Styczniowych w Szudziałowie. Pan dyrektor Grzegorz Zalewski wraz z wicedyrektorem Lechem Czarnowiczem i kierownikiem Pracowni Ćwiczeń Technicznych Panem Mirosławem Osialem przedstawili uczniom ofertę edukacyjną szkoły, realizowane projekty praktyk zagranicznych na Malcie i praktyk krajowych w ramach programu "Kompetentni i wykwalifikowani".
-
-Uczniowie zwiedzili pracownie przedmiotowe i zawodowe oraz powstającą pracownię obrabiarek sterowanych numerycznie (CNC), gdzie pan Marek Mozyrski przeprowadził krótkie warsztaty komputerowego rysunku i modelowania. Dziewczyny z wielkim zaangażowaniem uczestniczyły w warsztatach tworzenia kul do kąpieli, chętne mogły skorzystać z wybranych zabiegów fryzjerskich, dzięki uprzejmości naszych dziewczyn z klas fryzjerskich i nauczycielek przedmiotów zawodowych kierunku technik usług fryzjerskich: pań Elwiry Zabłockiej i Karoliny Roszkowskiej. Bardzo dziękujemy uczniom z Szudziałowa za przybycie do naszej szkoły i zapoznanie się z jej ofertą.</h5>
+			<div class="col s12 m12 6 l12 xl6">
+				<div class="card">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img src="../img/img3.jpg" class="activator">
+					</div>
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">
+							Wizyta uczniów Szkoły Podstawowej z Szudziałowa.
+						</span>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m12 6 l12 xl6">
+				<div class="card">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img src="../img/gos.jpg" class="activator">
+					</div>
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">
+							Wizyta uczniów Szkoły Podstawowej z Szudziałowa.
+						</span>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m12 6 l12 xl6">
+				<div class="card">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img src="../img/gos.jpg" class="activator">
+					</div>
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">
+							Wizyta uczniów Szkoły Podstawowej z Szudziałowa.
+						</span>
+					</div>
+				</div>
+			</div>
 
 		</div>
-			<div class="col s12 card white-text kuba">
-				<img src="https://farm8.staticflickr.com/7859/47421577181_a0738ebe8b_s.jpg" class="post">
-				<h3 class="letterSpac">Dzień Otwarty Szkoły</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac">22 marca 2019 roku Zespół Szkół Zawodowych im. Elizy Orzeszkowej w Sokółce odwiedziła młodzież ze Szkoły Podstawowej im. Bohaterów Powstań Narodowych i Gimnazjum im. Powstańców Styczniowych w Szudziałowie. Pan dyrektor Grzegorz Zalewski wraz z wicedyrektorem Lechem Czarnowiczem i kierownikiem Pracowni Ćwiczeń Technicznych Panem Mirosławem Osialem przedstawili uczniom ofertę edukacyjną szkoły, realizowane projekty praktyk zagranicznych na Malcie i praktyk krajowych w ramach programu "Kompetentni i wykwalifikowani".
 
-Uczniowie zwiedzili pracownie przedmiotowe i zawodowe oraz powstającą pracownię obrabiarek sterowanych numerycznie (CNC), gdzie pan Marek Mozyrski przeprowadził krótkie warsztaty komputerowego rysunku i modelowania. Dziewczyny z wielkim zaangażowaniem uczestniczyły w warsztatach tworzenia kul do kąpieli, chętne mogły skorzystać z wybranych zabiegów fryzjerskich, dzięki uprzejmości naszych dziewczyn z klas fryzjerskich i nauczycielek przedmiotów zawodowych kierunku technik usług fryzjerskich: pań Elwiry Zabłockiej i Karoliny Roszkowskiej. Bardzo dziękujemy uczniom z Szudziałowa za przybycie do naszej szkoły i zapoznanie się z jej ofertą.</h5>
-			</div>
-				<div class="col s12 card white-text kuba">
-				<img src="https://farm8.staticflickr.com/7859/47421577181_a0738ebe8b_s.jpg" class="post">
-				<h3 class="letterSpac">Kurłła kiedyś to było</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac">We czwartek, 21 marca 2019 roku odbył się w naszej szkole Dzień Otwarty. W tym roku szkolnym z powodu kończenia nauki przez uczniów klas III gimnazjum i VIII szkoły podstawowej goście przybyli liczniej niż to bywało w poprzednich latach. Przy wejściu do szkoły uczniowie i ich opiekunowie byli serdecznie witani przez wyznaczonych uczniów i nauczycieli, a następnie udawali się na zwiedzanie naszej szkoły. Gimnazjaliści i uczniowie klas ósmych z całego Powiatu Sokólskiego mogli zapoznać się z bogatą ofertą edukacyjną naszej szkoły na rok szkolny 2019/2020, obejrzeć na sali gimnastycznej stoiska poświęcone poszczególnym kierunkom kształcenia i wystawy firm współpracujących z naszą szkołą: "Metal-Fach", "Agromech" i "Jaskółka". Następnie goście zwiedzali budynek szkoły i Pracownie Ćwiczeń Technicznych. Po zwiedzaniu szkoły na górnym korytarzu wszystkich gości powitał pan dyrektor Grzegorz Zalewski, który poinformował o otwieraniu przez szkołę nowego kierunku - technik logistyk, a także o wzbogaceniu zawodu technik mechanik o specjalizację operator obrabiarek skrawających (CNC). Następnie przedstawił inne zawody funkcjonujące w naszej szkole od wielu lat - technik usług fryzjerskich ze specjalnością kosmetyka pielęgnacyjna i upiększająca, technik informatyk i branżową szkołę I stopnia z wieloma specjalnościami zawodowymi. Po wystąpieniu pana dyrektora głos zabrali pan wicedyrektor Lech Czarnowicz i pani Aneta Tymińska, którzy są szkolnymi koordynatorami projektów staży zagranicznych we Włoszech (Bolonia) i na Malcie w ramach "Programu Operacyjnego Wiedza Edukacja Rozwój". Zapoznali wszystkich ze szczegółami projektów realizowanych przez szkołę. Następnie odbył się bardzo interesujący pokaz makijażu i fryzur wieczorowych przygotowany przez uczennice profilu fryzjerskiego wraz z ich opiekunkami, paniami Elwirą Zabłocką i Karoliną Roszkowską. Na koniec tej części Dnia Otwartego się pokaz bokserski z udziałem zawodników UKS "Boxing" Sokółka, prowadzony przez Kamila Sołowieja, ucznia klasy III Technikum informatycznego. Na koniec Dnia Otwartego na bieżni wokół boiska szkolnego można było obejrzeć przejazdy gokartów przygotowywanych do tego przez zainteresowanych uczniów naszej szkoły.
 
-Wszystkim uczniom i nauczycielom, którzy zaangażowali się w organizację Dnia Otwartego, składamy serdeczne podziękowania. Dziękujemy również wszystkim uczniom gimnazjów i szkół podstawowych oraz ich opiekunom za przybycie do naszej szkoły i za zapoznanie się z jej ofertą edukacyjną.</h5>
-			</div>
-								<div class="col s12 card white-text kuba">
-				<img src="https://farm8.staticflickr.com/7859/47421577181_a0738ebe8b_s.jpg" class="post">
-				<h3 class="letterSpac">Kurłła kiedyś to było</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac">18 marca 2019 roku Zespół Szkół Zawodowych im. Elizy Orzeszkowej w Sokółce odwiedziła młodzież z klasy III Gimnazjum ze Szkoły Podstawowej Nr 2 z Oddziałami Integracyjnymi w Sokółce pod opieką pani Katarzyny Narbutt. Uczniowie zostali przywitani przez pana dyrektora Grzegorza Zalewskiego, a następnie zapoznani przez wicedyrektora szkoły, pana Lecha Czarnowicza i panią Anetę Tymińską z projektami unijnymi realizowanymi przez szkołę we Włoszech i na Malcie oraz z zajęciami w ramach projektu "Kompetentni i wykwalifikowani" realizowanego przez Starostwo Powiatowe w Sokółce.
-
-Następnie uczniowie udali się do Pracowni Ćwiczeń Technicznych, gdzie kierownik Mirosław Wojciech Osial zapoznał uczniów z możliwościami kształcenia praktycznego naszej szkoły i oprowadził ich po pracowniach, w których nasi uczniowie odbywają zajęcia praktyczne wykonując różne elementy metalowe na maszynach będących na wyposażeniu Pracowni. Pan Jerzy Szymaniuk zaprezentował działanie programów komputerowych uruchamianej pracowni obrabiarek typu CNC (obrabiarek sterowanych numerycznie). Na koniec odwiedzin uczniowie weszli do pracowni fryzjersko-kosmetycznej, gdzie panie Elwira Zabłocka i Karolina Roszkowska zapoznały ich z profilem fryzjersko-kosmetycznym szkoły, a następnie goście własnoręcznie wykonali z materiałów ekologicznych kule kąpielowe i skorzystali z wybranych zabiegów fryzjerskich.</h5>
-			</div>
-								<div class="col s12 card white-text kuba">
-				<img src="https://farm8.staticflickr.com/7859/47421577181_a0738ebe8b_s.jpg" class="post">
-				<h3 class="letterSpac">Kurłła kiedyś to było</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac">kurłłłłłaa Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
-			</div>
-				<div class="col s12 card white-text kuba">
-				<img src="https://farm8.staticflickr.com/7859/47421577181_a0738ebe8b_s.jpg" class="post">
-				<h3 class="letterSpac">Kurłła kiedyś to było</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac">kurłłłłłaa Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
-			</div>
-								<div class="col s12 card white-text kuba">
-				<img src="https://farm8.staticflickr.com/7859/47421577181_a0738ebe8b_s.jpg" class="post">
-				<h3 class="letterSpac">Kurłła kiedyś to było</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac">kurłłłłłaa Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
-			</div>
-								<div class="col s12 card white-text kuba">
-				<img src="https://farm8.staticflickr.com/7859/47421577181_a0738ebe8b_s.jpg" class="post">
-				<h3 class="letterSpac">Kurłła kiedyś to było</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac">kurłłłłłaa Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
-			</div>
-								<div class="col s12 card white-text kuba">
-				<img src="https://farm8.staticflickr.com/7859/47421577181_a0738ebe8b_s.jpg" class="post">
-				<h3 class="letterSpac">Kurłła kiedyś to było</h3>
-				<div class="background"></div>
-				<h5 class="letterSpac">kurłłłłłaa Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
-			</div>
-		</div>
-</div>
+	</div>
 </div>
 	<!-- end container -->
 	<?php
