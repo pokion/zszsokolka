25c9cfad858f4be71d73370f2d8eb23191494d6c
Patryk Olechno <patrykoolechno@gmail.com>
2019-04-18 12:43:26 +0200

    rekrutacja gimnazjum , do poprawy responsywana tabelka

--- html/rekrutacja_gim.php
+++ html/rekrutacja_gim.php
@@ -25,11 +25,9 @@
 				<br>
 				<br>
 
-				<table class="card">
+				<table class="responsive-table">
+					<h3>Nabór na rok szkolny 2019/2020</h3>
 					<thead>
-						<tr>
-							<th colspan="5"><h3 class="center-align">Nabór na rok szkolny 2019/2020</h3></th>
-						</tr>
 						<tr>
 							<th><h3 class="center-align">Typ Szkoły (cykl nauczania)</h3></th>
 							<th><h3 class="center-align">Profil kształcenia (numer zawodu)</h3></th>
@@ -54,14 +52,18 @@
 						<tr>
 							<td><h5 class="tableFont center-align">Technik informatyk</h5></td>
 							<td><h5 class="tableFont center-align">351203</h5></td>
+							<td><h5 class="tableFont center-align">x</h5></td>
 						</tr>
 						<tr>
 							<td><h5 class="tableFont center-align">Technik logistyk</h5></td>
 							<td><h5 class="tableFont center-align">333107</h5></td>
+							<td><h5 class="tableFont center-align">x</h5></td>
 						</tr>
 						<tr>
 							<td rowspan="2"><h5 class="tableFont center-align">3-letnia Branżowa Szkoła I stopnia</h5></td>
 							<td><h5 class="tableFont center-align">Oddział wielozawodowy</h5></td>
+							<td><h5 class="tableFont center-align">x</h5></td>
+							<td><h5 class="tableFont center-align">x</h5></td>
 						</tr>
 					</tbody>
 				</table>
@@ -73,11 +75,11 @@
 		<p><h3 class="center-align">Plakat i ulotka reklamowa</h3></p>
 		<br>
 		<div class="row">
-			<div class="col s12 centerFlex">
+			<div class="col s12 valign-wrapper">
 
-				<img class="center-align materialboxed" width="300" src="../img/ulotka2.jpg">
-				<img class="center-align materialboxed" width="300" src="../img/ulotka1.jpg">
-				<img class="center-align materialboxed" width="300" src="../img/ulotka3.jpg">
+				<img class="center-align materialboxed" width="100%" src="../img/ulotka2.jpg">
+				<img class="center-align materialboxed" width="100%" src="../img/ulotka1.jpg">
+				<img class="center-align materialboxed" width="100%" src="../img/ulotka3.jpg">
 
 			</div>
 		</div>
@@ -85,56 +87,160 @@
 		<br>
 		<!-- end plakat -->
 		<!-- filmiki -->
-		<div class="row">
-			<div class="col s4">
+		<div class="row center-align">
+			<div class="col s12 ">
 				<h3>Migawki filmowe z zajęć praktycznych i warsztatowych w naszej szkole</h3>
-				<iframe src="https://www.youtube.com/embed/-UPvYgzI3DY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				<div class="video-container">
+							<iframe width="600" height="480" src="https://www.youtube.com/embed/-UPvYgzI3DY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				</div>
 			</div>
-			<div class="col s4">
+			<div class="col s12">
 				<h3>Technik Informatyk - zajęcia z diagnostyki i naprawy urządzeń techniki komputerowej</h3>
-				<iframe src="https://www.youtube.com/embed/Gp_8lgPewjA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					<div class="video-container">
+				<iframe width="600" height="480" src="https://www.youtube.com/embed/Gp_8lgPewjA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					</div>
 			</div>
-			<div class="col s4">
+			<div class="col s12">
 				<h3>Filmik z zajęć na Pracowniach Ćwiczeń Technicznych - warsztaty szkolne</h3>
-				<iframe class="centerFlex" src="https://www.youtube.com/embed/GrgsJ6fD228" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+						<div class="video-container">
+				<iframe width="853" height="480" src="https://www.youtube.com/embed/GrgsJ6fD228" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+						</div>
 			</div>
 		</div>
 		<!-- end filmiki -->
-		<br>
-		<br>
 		<!-- kryteria -->
 		<div class="row">
 			<div class="col s12 card">
-				<h3 class="center-align">KRYTERIA REKRUTACJI DO KLAS PIERWSZYCH:</h3>
-				<br>
-				<br>
-				<h3 style="padding-left: 10px">I. Terminy przeprowadzania postępowania rekrutacyjnego, w tym terminy składania dokumentów do klas I publicznych szkół ponadgimnazjalnych, klasy I branżowej szkoły I stopnia:</h3>
-				<ul class="listStyle">
-					<li><h5>złożenie wniosku o przyjęcie do szkoły ponadgimnazjalnej oraz do branżowej szkoły I stopnia wraz z dokumentami potwierdzającymi spełnianie przez kandydata warunków lub kryteriów branych pod uwagę w postępowaniu rekrutacyjnym -  od 7 maja do 22 czerwca 2018 r. do godz. 15.00,</h5></li>
-					<li><h5>wydanie przez szkołę prowadzącą kształcenie zawodowe skierowania na badanie lekarskie kandydatowi, który złożył wniosek o przyjęcie do szkoły ponadgimnazjalnej oraz do branżowej szkoły I stopnia -  po złożeniu wniosku o przyjęcie do szkoły ponadgimnazjalnej oraz do branżowej szkoły I stopnia w postępowaniu rekrutacyjnym,</h5></li>
-					<li><h5>uzupełnienie wniosku o przyjęcie do szkoły ponadgimnazjalnej o świadectwo ukończenia gimnazjum oraz o zaświadczenie o wynikach egzaminu gimnazjalnego -  do 25 czerwca 2018 r. do godz. 15.00,</h5></li>
-					<li><h5>podanie do publicznej wiadomości przez komisję rekrutacyjną listy kandydatów zakwalifikowanych i kandydatów niezakwalifikowanych - 11 lipca 2018 r. o godz. 10.00,</h5></li>
-				</ul>
-				<h3 style="padding-left: 10px">II. Sposób przeliczania na punkty ocen z języka polskiego i trzech wybranych obowiązkowych zajęć edukacyjnych oraz wyników egzaminu przeprowadzonego w ostatnim roku nauki w gimnazjum, a także sposób punktowania innych osiągnięć kandydatów:</h3>
-				<ol type="1" >
-					<li><h5>Za wyniki egzaminu w ostatnim roku nauki w gimnazjum - max. 100 pkt
-					(Uzyskane wyniki procentowe z każdej części egzaminu (zakresu: języka polskiego; historii i wiedzy o społeczeństwie; matematyki; przedmiotów przyrodniczych: biologii, geografii, fizyki i chemii; języka obcego nowożytnego na poziomie podstawowym) mnożone są przez przelicznik 0,2, np. 75% = 75 x 0,2 = 15 pkt. Maksymalna liczba punktów za 5 wyników procentowych wynosi 500% x 0,2 = 100).</h5></li>
-					<li><h5>Za oceny z języka polskiego i trzech wybranych obowiązkowych zajęć edukacyjnych - max. 72 pkt:</h5></li>
-					<ul class="listStyle">
-						<li><h5>ocena celująca - 18 pkt</h5></li>
-						<li><h5>ocena celująca - 18 pkt</h5></li>
-						<li><h5>ocena celująca - 18 pkt</h5></li>
-						<li><h5>ocena celująca - 18 pkt</h5></li>
-						<li><h5>ocena celująca - 18 pkt</h5></li>
-					</ul>
-				</ol>
+				<h3 class="center-align">KRYTERIA REKRUTACJI DO KLAS PIERWSZYCH (PO GIMNAZJUM):</h3>
 
-			</div>
-		</div>
+			<b>I. Terminy przeprowadzania postępowania rekrutacyjnego, w tym terminy składania dokumentów do klas I technikum 4-letniego oraz klas I branżowej szkoły I stopnia:</b>
+			<ul>
+			  <li>
+					złożenie wniosku o przyjęcie do szkoły ponadgimnazjalnej oraz do branżowej szkoły I stopnia wraz z dokumentami potwierdzającymi spełnianie przez kandydata warunków lub kryteriów branych pod uwagę w postępowaniu rekrutacyjnym <b>od 13 maja do 18 czerwca 2019 r. do godz. 16.00</b>
+				</li>
+			  <li>
+					uzupełnienie wniosku o przyjęcie do szkoły ponadgimnazjalnej oraz branżowej szkoły I stopnia o świadectwo ukończenia gimnazjum oraz o zaświadczenie o wynikach egzaminu gimnazjalnego  <b>od 21 do 25 czerwca 2019 r. do godz. 16.00</b>
+				</li>
+			  <li>
+					weryfikacja przez komisję rekrutacyjną wniosków o przyjęcie do szkoły ponadgimnazjalnej i do branżowej szkoły I stopnia dokumentów potwierdzających spełnianie przez kandydata warunków lub kryteriów branych pod uwagę w postępowaniu rekrutacyjnym - <b>do 4 lipca 2019 r.</b>
+				</li>
+
+			  <li>
+					podanie do publicznej wiadomości przez komisję rekrutacyjną listy kandydatów zakwalifikowanych i kandydatów niezakwalifikowanych <b>5 lipca 2019 r. o godz. 10.00</b>
+				</li>
+			  <li>
+					wydanie przez szkołę prowadzącą kształcenie zawodowe skierowania na badanie lekarskie - <b>do 8 lipca 2019 r. do godz. 16.00</b>
+				</li>
+			  <li>
+					potwierdzenie przez rodzica kandydata albo kandydata pełnoletniego woli przyjęcia w postaci przedłożenia oryginału świadectwa ukończenia gimnazjum i oryginału zaświadczenia o wynikach egzaminu gimnazjalnego, o ile nie zostały one złożone w uzupełnieniu wniosku, a także zaświadczenia lekarskiego zawierającego orzeczenie o braku przeciwwskazań zdrowotnych do podjęcia praktycznej nauki zawodu - <b>do 10 lipca 2019 r. do godz. 16.00</b></li>
+			  <li>
+					podanie do publicznej wiadomości przez komisję rekrutacyjną listy kandydatów przyjętych i kandydatów nieprzyjętych - <b>12 lipca 2019 r. o godz. 10.00</b></li>
+			  <li>
+					poinformowanie przez dyrektora szkoły ponadgimnazjalnej kuratora oświaty o liczbie wolnych miejsc w szkole - <b>12 lipca 2019 r. o godz. 14.30.</b>
+				</li>
+			</ul>
+
+			<b>II. Sposób przeliczania na punkty ocen z języka polskiego i trzech wybranych obowiązkowych zajęć edukacyjnych oraz wyników egzaminu przeprowadzonego w ostatnim roku nauki w gimnazjum, a także sposób punktowania innych osiągnięć kandydatów:</b><br>
+			<br>
+			<u>Maksymalna liczba punktów możliwych do uzyskania wynosi:</u>
+			<ol>
+			  <li>Za wyniki egzaminu w ostatnim roku nauki w gimnazjum - max. 100 pkt<br>(Uzyskane wyniki procentowe z każdej części egzaminu (zakresu: języka polskiego; historii i wiedzy o społeczeństwie; matematyki; przedmiotów przyrodniczych: biologii, geografii, fizyki i chemii; języka obcego nowożytnego na poziomie podstawowym) mnożone są przez przelicznik 0,2, np. 75% = 75 x 0,2 = 15 pkt. Maksymalna liczba punktów za 5 wyników procentowych wynosi 500% x 0,2 = 100).</li>
+			  <li>Za oceny z języka polskiego i trzech wybranych obowiązkowych zajęć edukacyjnych - max. 72 pkt:</li>
+			  <ul>
+			    <li>ocena celująca - 18 pkt</li>
+			    <li>ocena bardzo dobra - 17 pkt</li>
+			    <li>ocena dobra - 14 pkt</li>
+			    <li>ocena dostateczna - 8 pkt</li>
+				<li>ocena dopuszczająca - 2 pkt</li>
+			  </ul>
+			</ol>
+			<p>Punkty za oceny przyznawane są z czterech przedmiotów:<br>
+				W Technikum:</p>
+			<ul>
+			  <li>dla zawodu technik mechanik: język polski, język obcy obowiązkowy, matematyka, zajęcia techniczne</li>
+			  <li>dla zawodu technik informatyk: język polski, język obcy obowiązkowy, matematyka, informatyka</li>
+			  <li>dla zawodu technik usług fryzjerskich: język polski, język obcy obowiązkowy, matematyka, biologia</li>
+			  <li>dla zawodu technik logistyk: język polski, język obcy obowiązkowy, matematyka, geografia</li>
+			</ul>
+			<p>W Branżowej Szkole I stopnia:</p>
+			<ul>
+			    <li>dla oddziału wieolozawodowego: język polski, język obcy obowiązkowy, matematyka, geografia</li>
+			</ul>
+			<p>3. Inne osiągnięcia ucznia - max. 28 pkt (za konkursy wymienione w podpunktach b-f - 18 max. pkt):</p>
+			<ul>
+			  <ul>
+			    <li>ukończenie gimnazjum z wyróżnieniem - 7 pkt</li>
+			    <li>uzyskanie w zawodach wiedzy będących konkursem o zasięgu ponadwojewódzkim organizowanym przez kuratorów oświaty:</li>
+			    <ul>
+			      <li>tytułu finalisty konkursu przedmiotowego - 10 pkt</li>
+			      <li>tytułu laureata konkursu tematycznego lub interdyscyplinarnego - 7 pkt</li>
+				  <li>tytułu finalisty konkursu tematycznego lub interdyscyplinarnego - 5 pkt</li>
+			    </ul>
+			    <li>uzyskanie w zawodach wiedzy będących konkursem o zasięgu międzynarodowym lub ogólnopolskim albo turniejem o zasięgu ogólnopolskim:</li>
+			    <ul>
+			      <li>tytułu finalisty konkursu z przedmiotu lub przedmiotów artystycznych objętych ramowym planem nauczania szkoły artystycznej - 10 pkt</li>
+			      <li>tytułu finalisty konkursu z przedmiotu lub przedmiotów artystycznych nieobjętych ramowym planem nauczania szkoły artystycznej - 4 pkt</li>
+			      <li>tytułu finalisty turnieju z przedmiotu lub przedmiotów artystycznych objętych ramowym planem nauczania szkoły artystycznej - 3 pkt</li>
+			    </ul>
+				<li>uzyskanie w zawodach wiedzy będących konkursem o zasięgu wojewódzkim organizowanym przez kuratora oświaty:</li>
+			    <ul>
+			      <li>dwóch lub więcej tytułów finalisty konkursu przedmiotowego - 10 pkt</li>
+			      <li>dwóch lub więcej tytułów laureata konkursu tematycznego lub interdyscyplinarnego - 7 pkt</li>
+			      <li>dwóch lub więcej tytułów finalisty konkursu tematycznego lub interdyscyplinarnego - 5 pkt</li>
+			      <li>tytułu finalisty konkursu przedmiotowego - 7 pkt</li>
+				  <li>tytułu laureata konkursu tematycznego lub interdyscyplinarnego - 5 pkt</li>
+				  <li>tytułu finalisty konkursu tematycznego lub interdyscyplinarnego - 3 pkt</li>
+			    </ul>
+				<li>uzyskanie w zawodach wiedzy będących konkursem albo turniejem, o zasięgu ponadwojewódzkim lub wojewódzkim:</li>
+			    <ul>
+			      <li>dwóch lub więcej tytułów finalisty konkursu z przedmiotu lub przedmiotów artystycznych objętych ramowym planem nauczania szkoły artystycznej - 10 pkt</li>
+			      <li>dwóch lub więcej tytułów laureata turnieju z przedmiotu lub przedmiotów artystycznych nie objętych ramowym planem nauczania szkoły artystycznej - 7 pkt</li>
+			      <li>dwóch lub więcej tytułów finalisty turnieju z przedmiotu lub przedmiotów artystycznych nie objętych ramowym planem nauczania szkoły artystycznej - 5 pkt</li>
+			      <li>tytułu finalisty konkursu przedmiotu lub przedmiotów artystycznych objętych ramowym planem nauczania szkoły artystycznej - 7 pkt</li>
+				  <li>tytułu laureata turnieju przedmiotu lub przedmiotów artystycznych nieobjętych ramowym planem nauczania szkoły artystycznej - 3 pkt</li>
+				  <li>tytułu finalisty turnieju przedmiotu lub przedmiotów artystycznych nieobjętych ramowym planem nauczania szkoły artystycznej - 2 pkt</li>
+			    </ul>
+				<li>uzyskanie wysokiego miejsca w zawodach wiedzy innych niż wymienione powyżej, artystycznych lub sportowych, organizowanych przez kuratora oświaty lub inne podmioty działające na terenie szkoły, na szczeblu:</li>
+			    <ul>
+			      <li>międzynarodowym - 4 pkt</li>
+			      <li>krajowym - 3 pkt</li>
+			      <li>wojewódzkim - 2 pkt</li>
+			      <li>powiatowym - 1 pkt</li>
+			    </ul>
+				<li>W przypadku gdy kandydat ma więcej niż jedno szczególne osiągnięcie z takich samych zawodów wiedzy, artystycznych i sportowych, na tym samym szczeblu oraz z tego samego zakresu, wymienione na świadectwie ukończenia szkoły podstawowej, przyznaje się jednorazowo punkty za najwyższe osiągnięcie tego ucznia w tych zawodach, z tym że maksymalna liczba punktów możliwych do uzyskania za wszystkie osiągnięcia wynosi 18 punktów.</li>
+			    <li>za osiągnięcia w zakresie aktywności społecznej, w tym na rzecz środowiska szkolnego, w szczególności w formie wolontariatu - 3 pkt</li>
+			  </ul>
+			  <li>Laureaci konkursów wiedzy o zasięgu wojewódzkim i ponadwojewódzkim, których program obejmuje  w  całości  lub  poszerza  treści  podstawy  programowej  co  najmniej  z  jednego przedmiotu, przyjmowani są do wybranej szkoły ponadpodstawowej w pierwszej kolejności.</li>
+			  <li>O przyjęciu kandydata do szkoły decyduje łączna liczba punktów uzyskanych w postępowaniu kwalifikacyjnym  za oceny i  inne osiągnięcia ucznia wymienione na świadectwie ukończenia szkoły podstawowej oraz uzyskanych za wyniki egzaminu przeprowadzonego w ostatnim roku nauki w szkole.</li>
+			  <li>W  sytuacji  gdy  liczba  kandydatów  jest  równa  lub  mniejsza  od  liczby  miejsc  jaką  dysponuje szkoła  dyrektor  może  przyjąć  wszystkich  kandydatów  bez    przeprowadzania  postępowania  kwalifikacyjnego, jeśli spełniają  warunek braku przeciwwskazań zdrowotnych do kształcenia w określonym zawodzie.</li>
+			  <li>W  przypadku  uzyskania  równorzędnych  wyników  w  pierwszym  etapie  postepowania rekrutacyjnego,  na  drugim  etapie  postepowania  rekrutacyjnego  wybiera  się  kandydatów  z problemami zdrowotnymi, ograniczającymi możliwość wyboru kierunku kształcenia ze względu na stan  zdrowia,  potwierdzonymi  opinią  publicznej  poradni  psychologiczno-pedagogicznej,  nie kolidującymi z zaświadczeniem lekarskim zawierającym orzeczenie o braku przeciwwskazań zdrowotnych do podjęcia praktycznej nauki zawodu</li>
+			  <li>W  przypadku  uzyskania  równorzędnych  wyników  w  drugim  etapie  postepowania rekrutacyjnego, na trzecim  etapie postepowania rekrutacyjnego brane są pod uwagę następujące kryteria:</li>
+			  <ul>
+			<li>niepełnosprawność kandydata, </li>
+			<li>niepełnosprawność jednego z rodziców kandydata, </li>
+			<li>niepełnosprawność obojga rodziców kandydata, </li>
+			<li>niepełnosprawność rodzeństwa kandydata, </li>
+			<li>wielodzietność rodziny kandydata(oznacza to rodzinę wychowującą troje lub więcej dzieci), </li>
+			<li>samotne wychowywanie kandydata w rodzinie, </li>
+			<li>objęcie kandydata pieczą zastępczą.</li>
+			</ul>
+			<p>Wszystkie te kryteria mają jednakową wartość.</p>
+			  </ul>
+			<b>III. Kandydat do szkoły składa następujące dokumenty:</b>
+			<ul>
+			  <li>podanie o przyjęcie do szkoły z określeniem profilu lub zawodu (do pobrania w sekretariacie szkoły lub ze strony www)</li>
+			  <li>świadectwo ukończenia gimnazjum</li>
+			  <li>zaświadczenie o szczegółowych wynikach egzaminu gimnazjalnego</li>
+			  <li>2 zdjęcia</li>
+			  <li>zaświadczenie lekarskie o braku przeciwwskazań do kształcenia w określonym zawodzie (skierowanie na badania wydawane jest w sekretariacie szkoły po podaniu do publicznej wiadomości przez komisję rekrutacyjną listy kandydatów zakwalifikowanych i kandydatów niezakwalifikowanych)</li>
+			</ul>
+
 		<!-- end kryteria -->
-		<br>
-		<br>
+
 	</div>
+</div>
+</div>
+
 
 	<?php
 		include_once('includes/footer.php');
